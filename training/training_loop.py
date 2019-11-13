cc74b894ed9552293ac1430a06b490c62526ff3f
Diego Porres <daporres@ufm.edu>
2019-11-13 17:10:30 +0100

    Change learning rates

--- training/training_loop.py
+++ training/training_loop.py
@@ -264,6 +264,7 @@ def training_loop(
             if cur_tick % network_snapshot_ticks == 0 or done or cur_tick == 1:
                 pkl = os.path.join(submit_config.run_dir, 'network-snapshot-%06d.pkl' % (cur_nimg // 1000))
                 misc.save_pkl((G, D, Gs), pkl)
+                # FID50k is only time-consuming, and rarely gives good info, so we won't calculate it:
                 # metrics.run(pkl, run_dir=submit_config.run_dir, num_gpus=submit_config.num_gpus, tf_config=tf_config)
 
             # Update summaries and RunContext.
